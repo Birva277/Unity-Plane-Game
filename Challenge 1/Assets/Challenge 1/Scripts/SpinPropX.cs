0f79e1b88aeb85b0fb967224b36477fef4634a2e
Birva277 <55867337+Birva277@users.noreply.github.com>
2020-05-14 10:16:19 +0530

    Committed
    Plane Unity project is added to Github

---
+++ Challenge 1/Assets/Challenge 1/Scripts/SpinPropX.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinPropX : MonoBehaviour
+{
+    public float vertiInput;
+    public float rotSpeed=20f;
+    public float speed = 0;
+    public float forwardInput=0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        vertiInput = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.forward * rotSpeed * vertiInput);
+    }
+}
